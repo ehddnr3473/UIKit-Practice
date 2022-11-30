ae71ee9a022e49af6989fe70db1dd13bfd0ebf2c
Donguk Kim <ehddnr73@naver.com>
2022-11-30 10:10:11 +0900

    Playground project PlayOn_UIKit 생성

---
+++ PlayOn_UIKit/PlayOn_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlayOn_UIKit
+//
+//  Created by 김동욱 on 2022/11/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
