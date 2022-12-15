4d841c49256268b4ed0435a20c153d84994e977a
Donguk Kim <ehddnr73@naver.com>
2022-12-16 07:07:22 +0900

    preview 수정

---
+++ PlayOn_UIKit/PlayOn_UIKit/Findings/ProgressIndicators/JGProgressHUDIndicators.swift
@@ -0,0 +1,29 @@
+//
+//  JGProgressHUDIndicators.swift
+//  PlayOn_UIKit
+//
+//  Created by 김동욱 on 2022/12/16.
+//
+
+import UIKit
+
+class JGProgressHUDIndicators: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
