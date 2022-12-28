596b61967d4dc0b21ad94473c335992d4ffc3a3a
Donguk Kim <ehddnr73@naver.com>
2022-12-28 16:39:11 +0900

    RawData 생성

---
+++ PlayOn_UIKit/PlayOn_UIKit/Findings/Diffable DataSource/DrinksRowData.swift
@@ -0,0 +1,28 @@
+//
+//  DrinksRowData.swift
+//  PlayOn_UIKit
+//
+//  Created by 김동욱 on 2022/12/28.
+//
+
+import Foundation
+
+let drinksRowData = """
+Americano
+Ade
+Apple juice
+Aloe juice
+Beer
+Coke
+Latte
+Mocha
+Soda
+Soju
+Smoothie
+Grape Juice
+Orange juice
+Makgeolli
+Wine
+Water
+Whiskey
+"""
