bb4f05a50088c085f1797d5df62e6d19539cd244
Donguk Kim <ehddnr73@naver.com>
2022-12-28 17:46:10 +0900

    RowData 수정

--- PlayOn_UIKit/PlayOn_UIKit/Findings/Diffable DataSource/DrinksRowData.swift
+++ PlayOn_UIKit/PlayOn_UIKit/Findings/Diffable DataSource/DrinksRowData.swift
@@ -8,21 +8,21 @@
 import Foundation
 
 let drinksRowData = """
-Americano
-Ade
-Apple juice
-Aloe juice
-Beer
-Coke
-Latte
-Mocha
-Soda
-Soju
-Smoothie
-Grape Juice
-Orange juice
-Makgeolli
-Wine
-Water
-Whiskey
+Americano,500
+Ade,500
+Apple juice,300
+Aloe juice,300
+Beer,1000
+Coke,500
+Latte,400
+Mocha,400
+Soda,800
+Soju,200
+Smoothie,400
+Grape Juice,300
+Orange juice,300
+Makgeolli,500
+Wine,400
+Water,1000
+Whiskey,400
 """
